2d95338084f6694575d354c6ca11046ce19323bc
LeoNielsen <leo.nielsen21@gmail.com>
2025-02-20 14:58:33 +0100

    url

--- src/API/Api.tsx
+++ src/API/Api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://backend:8080'; //'http://localhost:8080'
+const BASE_URL = 'http://localhost:8080' //'http://backend:8080'
 
 const getAllPlayers = async () => {
     try {
