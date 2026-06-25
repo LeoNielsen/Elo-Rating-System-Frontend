d28a3c08796ee0afc53a8afa61f47da8b007aba4
LeoNielsen <leo.nielsen21@gmail.com>
2026-06-25 22:01:08 +0200

    clean up

---
+++ eslint.config.ts
@@ -0,0 +1,34 @@
+// eslint.config.ts
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      react,
+    },
+
+    rules: {
+      // React plugin rules
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
